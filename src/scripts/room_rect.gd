af623ed73bc801b738e4a0215218c62aca362053
Steaf23 <steafmobile@gmail.com>
2026-04-13 20:31:50 +0200

    missing files

---
+++ src/scripts/room_rect.gd
@@ -0,0 +1,44 @@
+class_name RoomRect
+extends Node2D
+
+signal room_entered()
+signal room_exited()
+
+
+func _ready() -> void:
+	var bounds = global_bounds()
+	$Area2D.show()
+	%RoomShape.debug_color = Color(randf(), randf(), randf(), %RoomShape.debug_color.a)
+	
+	%RoomShape.shape.size = bounds.size
+	%RoomShape.position = bounds.position + bounds.size / 2 
+	print(%RoomShape.position)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is not PlayerStateMachine:
+		return
+	
+	room_entered.emit()
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is not PlayerStateMachine:
+		return
+	
+	room_exited.emit()
+
+
+func global_bounds() -> Rect2:
+	var global_min = Vector2()
+	global_min.x = min(%Corner1.position.x, %Corner2.position.x)
+	global_min.y = min(%Corner1.position.y, %Corner2.position.y)
+	
+	var global_max = Vector2()
+	global_max.x = max(%Corner1.position.x, %Corner2.position.x)
+	global_max.y = max(%Corner1.position.y, %Corner2.position.y)
+	
+	return Rect2(global_min, global_max - global_min)
+
+
+func _to_string() -> String:
+	return str(global_bounds())
