3a16c429020388f86b0dffea22943e1aeb66e587
Steaf23 <steafmobile@gmail.com>
2026-04-14 00:49:42 +0200

    - added smooth camera transition between rooms

--- src/scripts/rooms.gd
+++ src/scripts/rooms.gd
@@ -1,15 +1,32 @@
 extends Node2D
 
+var first_time: bool = true
 
 func _ready() -> void:
+	%Camera2D.position_smoothing_enabled = false
+	
 	for c in get_children():
 		if c is RoomRect:
 			c.room_entered.connect(_on_room_entered.bind(c))
 			
+			
 func _on_room_entered(rect: RoomRect) -> void:
-	print("entered: ", rect)
 	var bounds = rect.global_bounds()
-	%Camera2D.limit_top = bounds.position.y
-	%Camera2D.limit_left = bounds.position.x
-	%Camera2D.limit_bottom = bounds.end.y
-	%Camera2D.limit_right = bounds.end.x
+	
+	var camera = %Camera2D
+	var prev_pos = camera.get_screen_center_position()
+	
+	camera.limit_top = bounds.position.y
+	camera.limit_left = bounds.position.x
+	camera.limit_bottom = bounds.end.y
+	camera.limit_right = bounds.end.x
+	
+	if first_time:
+		first_time = false
+		return
+	
+	camera.offset = prev_pos - camera.get_screen_center_position()
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(camera, "offset", Vector2.ZERO, 0.6)
