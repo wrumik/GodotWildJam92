7d2deac88639f3eca80c5fad0e4fbb3c13f63d73
Steaf23 <steafmobile@gmail.com>
2026-04-16 20:10:49 +0200

    fix: improved enemy path finding

--- src/scripts/enemies/grid_mover.gd
+++ src/scripts/enemies/grid_mover.gd
@@ -3,11 +3,45 @@ extends Node
 
 @export var body: CharacterBody2D
 @export var grid_size: int = 8
+@export var move_speed: float = 65.0
 
-var target_position: Vector2
+var direction: Vector2i = Vector2.ZERO
+var current_target: Vector2i = Vector2.ZERO
+var angle: float = 0.0
 
 func _ready() -> void:
 	assert(body)
 	
 	body.global_position = body.global_position.snapped(Vector2(grid_size, grid_size))
-	target_position = body.global_position
+	current_target = body.global_position
+
+
+func _physics_process(delta: float) -> void:
+	if not body:
+		return
+		
+	var can_move = Vector2i(body.global_position) == current_target
+	
+	if can_move:
+		current_target += direction * grid_size
+	
+	body.global_position = body.global_position.move_toward(current_target, move_speed * delta)
+
+
+func pick_direction(to: Vector2) -> void:
+	if to == body.global_position:
+		direction = Vector2i.ZERO
+		return
+	
+	var raw_dir = body.global_position.direction_to(to)
+	angle = fposmod(raw_dir.angle(), TAU)
+	if angle < 1 * (PI/4):
+		direction = Vector2i.RIGHT
+	elif angle < 3 * (PI/4):
+		direction = Vector2i.DOWN
+	elif angle < 5 * (PI/4):
+		direction = Vector2i.LEFT
+	elif angle < 7 * (PI/4):
+		direction = Vector2i.UP
+	else:
+		direction = Vector2i.RIGHT
--- src/scripts/navigation_layer.gd
+++ src/scripts/navigation_layer.gd
@@ -5,13 +5,14 @@ func create_grid_for_room(room: RoomRect) -> AStarGrid2D:
 	var pixel_bounds = room.global_bounds()
 	grid.region = Rect2(local_to_map(to_local(pixel_bounds.position)), local_to_map(to_local(pixel_bounds.size)))
 	grid.cell_size = tile_set.tile_size
+	grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	grid.update()
 	
 	grid.fill_solid_region(grid.region, true)
-	for y in range(grid.region.position.y, grid.region.end.y):
-		for x in range(grid.region.position.x, grid.region.end.x):
+	for y in grid.region.size.y:
+		for x in grid.region.size.x:
 			var id = Vector2i(x, y)
 			if get_cell_source_id(grid.region.position + id) != -1:
-				grid.set_point_solid(Vector2i(x, y), false)
+				grid.set_point_solid(Vector2i(x, y) + grid.region.position, false)
 	
 	return grid
