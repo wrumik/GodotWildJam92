d12aba58ea01223ef1de95661e192458e53eda2f
wrum <walbrisch@gmail.com>
2026-04-16 16:25:54 +0200

    added some functionality to the main menu and setup the project for export

--- src/scripts/main_menu.gd
+++ src/scripts/main_menu.gd
@@ -1,11 +1,19 @@
 extends Control
 
+@onready var title_label: Label = $MarginContainer/TitleLabel
+@onready var options_menu: Control = $MarginContainer/OptionsMenu
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://src/scenes/main.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_options_button_pressed():
 	pass
+
+
+func _on_credits_button_pressed():
+	pass
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
