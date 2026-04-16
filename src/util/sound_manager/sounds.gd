16f4a9f936a885224dd3898902afc734864342dc
wrum <walbrisch@gmail.com>
2026-04-16 15:58:44 +0200

    fixed wrong footstep sound path

--- src/util/sound_manager/sounds.gd
+++ src/util/sound_manager/sounds.gd
@@ -6,7 +6,7 @@ const UI_FORWARD: StringName = "res://src/sound/UI_forward.wav"
 const UI_BACKWARD: StringName = "res://src/sound/UI_backward.wav"
 
 # GENERAL SFX
-const FOOTSTEP: StringName = "res://src/sound/UI_forward.wav"
+const FOOTSTEP: StringName = "res://src/sound/footstep.wav"
 const SWORD_SWOOSH: StringName = "res://src/sound/sword_swoosh.wav"
 const BUSH_SLICED: StringName = "res://src/sound/bush_sliced.wav"
 const GAIN_ITEM: StringName = "res://src/sound/gain_item.wav"
