3017a70310c88c2e83f115b6dd477eb47954b8d1
wrum <walbrisch@gmail.com>
2026-04-19 18:43:09 +0200

    added win condition

--- src/autoloads/game_data.gd
+++ src/autoloads/game_data.gd
@@ -1,3 +1,7 @@
 extends Node
 
 var ingredients_collected: int = 0
+
+func _process(delta: float) -> void:
+	if ingredients_collected >= 4:
+		get_tree().change_scene_to_file("res://src/scenes/game_end_scene.tscn")
